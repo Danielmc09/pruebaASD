65ce97dfbb8593f6f11d26fdf3f7cb0ecbc227ed
Danielmc09 <admendieta9@misena.edu.co>
2022-03-05 15:57:56 -0500

    install DjangoRestFramework and agregate in settings, and agregate requirements.txt

--- activosasd/activosasd/settings.py
+++ activosasd/activosasd/settings.py
@@ -37,12 +37,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #app3ros
+    'rest_framework',
     #myapp
     'app.activos',
     'app.area',
     'app.persona',
     'app.ubicacion',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
